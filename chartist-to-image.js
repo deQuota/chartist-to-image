94ffe9d24c584ab15a18f77e8f3ed32f827e75cd
deQuota <nisalwaduge@gmail.com>
2018-01-24 18:23:53 +0530

    added strict

--- chartist-to-image.js
+++ chartist-to-image.js
@@ -1,45 +1,49 @@
 ( function (root, factory) {
-        if (typeof define === "function" && define.amd) {
-            // AMD. Register as an anonymous module unless amdModuleId is set
-            define("chartist2image", [], function () {
-                return (root["chartist2image"] = factory());
-            });
-        } else if (typeof module === "object" && module.exports) {
-            // Node. Does not work with strict CommonJS, but
-            // only CommonJS-like environments that support module.exports,
-            // like Node.
-            module.exports = factory();
-        } else {
-            root["chartist2image"] = factory();
-        }
-    }
-(this, function () {
-
-    var domtoimage = require('dom-to-image');
-    var chartist2image = {
-        vaersion: "0.0.1"
-    };
-    
-    chartist2image.toJpeg = function (divID) {
-        domtoimage.toJpeg(document.getElementById(divID), { quality: 0.95, bgcolor: "#ffffff" })
-            .then( function(dataUrl){
-            var img = new Image();
-        img.src = dataUrl;
-        // Logger.log("Base 64 >>>>",img.src);
-        // document.body.appendChild(img);
-         var link = document.createElement("a");
-         link.download = divID+".jpeg";
-         link.href = dataUrl;
-         link.click();
-        console.log({ title: "pie-chart", content: img.src });
-
-
-    },
-        function(error){
-
-            console.log("Error occurred while rendering the Graph >>>>", error);
+    if (typeof define === "function" && define.amd) {
+        // AMD. Register as an anonymous module unless amdModuleId is set
+        define("chartist2image", [], function () {
+            return (root["chartist2image"] = factory());
         });
-    };
+    } else if (typeof module === "object" && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        root["chartist2image"] = factory();
+    }
+}(this, function () {
+
+
+    (function (window, document) {
+        'use strict';
+
+
+        var domtoimage = require('dom-to-image');
+        var chartist2image = {
+            vaersion: "0.0.1"
+        };
+
+        chartist2image.toJpeg = function (divID) {
+            domtoimage.toJpeg(document.getElementById(divID), {quality: 0.95, bgcolor: "#ffffff"})
+                .then(function (dataUrl) {
+                        var img = new Image();
+                        img.src = dataUrl;
+                        document.body.appendChild(img);
+                        var link = document.createElement("a");
+                        link.download = divID + ".jpeg";
+                        link.href = dataUrl;
+                        link.click();
+                        console.log({title: "pie-chart", content: img.src});
+
+
+                    },
+                    function (error) {
+
+                        console.log("Error occurred while rendering the Graph >>>>", error);
+                    });
+        };
+    }(window, document));
 
     return chartist2image;
 
